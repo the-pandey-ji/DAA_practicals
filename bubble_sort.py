5c66b379d916bbad6aac9608ffabf32738c0c3c7
the-pandey-ji <sonu.pandey_cs18@gla.ac.in>
2020-07-25 18:54:04 +0530

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+arr = [40, 5, 86, 32, 100, 25]
+print("Unsorted array")
+print(arr)
+
+# Traverse through all array elements
+for i in range(len(arr)):
+    for j in range(0, len(arr)-i-1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array")
+print(arr)
